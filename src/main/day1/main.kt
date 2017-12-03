92eeecf8a9ed12344d254c46dee177f4f6b2a04e
voddan <dgvodopyan@gmail.com>
2017-12-03 17:43:42 +0300

    day 1 task B spek

--- src/main/day1/main.kt
+++ src/main/day1/main.kt
@@ -7,7 +7,14 @@ fun taskA(input: String): Int {
     return sum
 }
 
+fun taskB(input: String): Int {
+    val digits = input.map { (it - '0').toInt() }
+    val pairs = (digits + digits.first()).zipWithNext()
+    val sum = pairs.filter { it.first == it.second }.sumBy { it.first }
+    return sum
+}
+
 fun main(args: Array<String>) {
-    taskA("1122")
+    taskB("1122")
 }
 
