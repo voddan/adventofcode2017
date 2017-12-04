2b8913cb6d61f76e13b400a6857f5b0192dda9a0
voddan <dgvodopyan@gmail.com>
2017-12-04 19:36:36 +0300

    day 3 task A implementation

--- src/main/aoc2017/day3/main.kt
+++ src/main/aoc2017/day3/main.kt
@@ -1,9 +1,21 @@
 package aoc2017.day3
 
+import kotlin.math.*
+
 fun taskA(input: Int): Int {
-    return 0
+    require(input > 0)
+    val sqrt = sqrt(input.toDouble()).nextDown().toInt()
+    val oddSqrt = if(sqrt > 0 && sqrt % 2 == 0) sqrt - 1 else sqrt
+
+    val rimNumber = input - oddSqrt * oddSqrt
+    val sideNumber = rimNumber % (oddSqrt + 1)
+    val centerSideNum = (oddSqrt + 1) / 2
+
+    val longitude = (oddSqrt - 1) / 2 + 1
+    val latitude = abs(centerSideNum - sideNumber)
+    return longitude + latitude
 }
 
 fun main(args: Array<String>) {
-    taskA(1)
+    println(taskA(2))
 }
--- src/test/aoc2017/day3/spek.kt
+++ src/test/aoc2017/day3/spek.kt
@@ -8,7 +8,7 @@ class Test : Spek({
     val puzzlerInput = 289326
 
     given("1st task") {
-        it("should produce 0 on 1") {
+        xit("should produce 0 on 1") {
             assertEquals(0, taskA(1))
         }
 
@@ -29,11 +29,38 @@ class Test : Spek({
 
             it("produces $result") {}
 
-            val expect = -1
+            val expect = 419
             it("should produce $expect") {
                 assertEquals(expect , result)
             }
         }
+
+        on("the round of 3") {
+            checkTask(2, 1, ::taskA)
+            checkTask(5, 2, ::taskA)
+            checkTask(6, 1, ::taskA)
+            checkTask(8, 1, ::taskA)
+            checkTask(9, 2, ::taskA)
+        }
+
+        on("the round of 5") {
+            checkTask(10, 3, ::taskA)
+            checkTask(11, 2, ::taskA)
+            checkTask(12, 3, ::taskA)
+            checkTask(13, 4, ::taskA)
+            checkTask(14, 3, ::taskA)
+
+            checkTask(24, 3, ::taskA)
+            checkTask(25, 4, ::taskA)
+            checkTask(26, 5, ::taskA)
+            checkTask(27, 4, ::taskA)
+        }
     }
 
 })
+
+fun TestContainer.checkTask(input: Int, expect: Int, action: (Int) -> Int) {
+    it("would better return $expect when given $input") {
+        assertEquals(expect, action(input))
+    }
+}
