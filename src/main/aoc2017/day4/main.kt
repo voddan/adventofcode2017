d19e31a788c506c5c7d6c9336aafba2ede499f8e
voddan <dgvodopyan@gmail.com>
2017-12-11 11:43:51 +0300

    day 4 task A spek

---
+++ src/main/aoc2017/day4/main.kt
@@ -0,0 +1,15 @@
+package aoc2017.day4
+
+
+fun isValid(str: String): Boolean {
+    return true
+}
+
+fun taskA(input: List<String>): Int {
+    return 0
+}
+
+
+fun main(args: Array<String>) {
+    taskA(listOf("aa bb", "a b a c"))
+}
